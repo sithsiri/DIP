68765dac7677460cecf024c3496f10ce89e2edc0
sithsiri <sith@live.com>
2017-05-26 22:50:23 -0500

    literally just a simple comm socket rn
    i'm trying to get the hang of python here, give me a week or so

--- client.py
+++ client.py
@@ -1,7 +1,18 @@
 #This is the client, not the server
+#Python 3 compatible
+import time
+import socket
 
-print "DIP Client super-alpha"
-print "This literally does nothing."
-print "This program will close in 5 seconds"
+print("DIP Client super-alpha")
+
+s = socket.socket()         # Create a socket object
+host = socket.gethostname() #input("Server hostname: ")
+port = int(float(input("Server port: ")))
+
+s.connect((host, port))
+print(s.recv(1024))
+s.send(bytes(input("Send: "), "utf-8"))
+s.close                     # Close the socket when done
+print("Connection closed")
 
 time.sleep(5)
--- server.py
+++ server.py
@@ -1,7 +1,24 @@
 #This is the server, not the client
+#Python 3 compatible
+import time
+import socket
 
-print "DIP Server super-alpha"
-print "This literally does nothing."
-print "This program will close in 5 seconds"
+print("DIP Server super-alpha")
+
+time.sleep(5)
+
+s = socket.socket()         # Create a socket object
+host = socket.gethostname() # Get local machine name
+port = int(float(input("Port to run server on: ")))
+s.bind((host, port))        # Bind to the port
+
+s.listen(5)                 # Now wait for client connection.
+while True:
+   c, addr = s.accept()     # Establish connection with client.
+   print('Got connection from', addr)
+   c.send(bytes("Thank you for connecting, send something", "utf-8"))
+   print(c.recv(1024))
+   c.close()                # Close the connection
+print("Connection closed")
 
 time.sleep(5)
